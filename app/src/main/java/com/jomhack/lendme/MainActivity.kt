b550d0a53b72f6fb4223ea29902a54d97608c08a
aldimaulana <aldieemaulana@gmail.com>
2018-10-06 12:09:32 +0800

    Initial commit

---
+++ app/src/main/java/com/jomhack/lendme/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jomhack.lendme
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
