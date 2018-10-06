a4573bb6139789ca0d44ff5371c8d681bcbd1e96
aldimaulana <aldieemaulana@gmail.com>
2018-10-06 13:52:05 +0800

    Adding Bottom Bar Adding Icon of Bottom Bar

--- app/src/main/java/com/jomhack/lendme/activity/MainActivity.kt
+++ app/src/main/java/com/jomhack/lendme/activity/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.jomhack.lendme.activity
 
 import android.os.Bundle
+import android.view.View
 import com.jomhack.lendme.R
 import com.jomhack.lendme.base.BaseActivity
+import kotlinx.android.synthetic.main.toolbar_primary.*
 
 class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
+    }
+
+    private fun initView() {
+        buttonBackToolbar.visibility = View.GONE
     }
 
 }
