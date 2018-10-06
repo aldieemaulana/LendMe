38474c0cda7ffce9e952b8314f0fff7616ab146b
aldimaulana <aldieemaulana@gmail.com>
2018-10-06 14:24:16 +0800

    Set up fragment listener and view

--- app/src/main/java/com/jomhack/lendme/activity/MainActivity.kt
+++ app/src/main/java/com/jomhack/lendme/activity/MainActivity.kt
@@ -7,18 +7,28 @@ import android.view.MenuItem
 import android.view.View
 import com.jomhack.lendme.R
 import com.jomhack.lendme.base.BaseActivity
-import com.jomhack.lendme.fragment.BoardFragment
+import com.jomhack.lendme.fragment.*
 import com.jomhack.lendme.utils.Constants
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.toolbar_primary.*
 
 class MainActivity : BaseActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     private val boardFragment = BoardFragment()
+    private val friendsFragment = FriendsFragment()
+    private val investsFragment = InvestsFragment()
+    private val historyFragment = HistoryFragment()
+    private val settingFragment = SettingsFragment()
     private val fragmentManager = supportFragmentManager
-    private var activeFragment: Fragment = boardFragment
+    private var activeFragment : Fragment = boardFragment
 
     private fun initFragment() {
         setTitle(getString(R.string.title_board))
+
+        fragmentManager.beginTransaction().add(R.id.frameLayout, settingFragment, Constants.FRAGMENT.SETTINGS.NAME).hide(settingFragment).commit()
+        fragmentManager.beginTransaction().add(R.id.frameLayout, historyFragment, Constants.FRAGMENT.HISTORY.NAME).hide(historyFragment).commit()
+        fragmentManager.beginTransaction().add(R.id.frameLayout, investsFragment, Constants.FRAGMENT.INVESTS.NAME).hide(investsFragment).commit()
+        fragmentManager.beginTransaction().add(R.id.frameLayout, friendsFragment, Constants.FRAGMENT.FRIENDS.NAME).hide(friendsFragment).commit()
         fragmentManager.beginTransaction().add(R.id.frameLayout, boardFragment, Constants.FRAGMENT.BOARD.NAME).commit()
     }
 
@@ -32,8 +42,52 @@ class MainActivity : BaseActivity(), BottomNavigationView.OnNavigationItemSelect
 
                 return true
             }
+
+            R.id.navigation_friends -> {
+                setTitle(getString(R.string.title_frieds))
+                fragmentManager.beginTransaction().hide(activeFragment).show(friendsFragment).commit()
+                activeFragment = friendsFragment
+
+                return true
+            }
+
+            R.id.navigation_invests-> {
+                setTitle(getString(R.string.title_invests))
+                fragmentManager.beginTransaction().hide(activeFragment).show(investsFragment).commit()
+                activeFragment = investsFragment
+
+                return true
+            }
+
+            R.id.navigation_history -> {
+                setTitle(getString(R.string.title_history))
+                fragmentManager.beginTransaction().hide(activeFragment).show(historyFragment).commit()
+                activeFragment = historyFragment
+
+                return true
+            }
+
+            R.id.navigation_settings -> {
+                setTitle(getString(R.string.title_settings))
+                fragmentManager.beginTransaction().hide(activeFragment).show(settingFragment).commit()
+                activeFragment = settingFragment
+
+                return true
+            }
         }
+
         return false
+    }
+
+    private fun setListener() {
+        bottomNavigation.setOnNavigationItemSelectedListener(this)
+    }
+
+    private fun initView() {
+        buttonBackToolbar.visibility = View.GONE
+
+        initFragment()
+        setListener()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,12 +95,6 @@ class MainActivity : BaseActivity(), BottomNavigationView.OnNavigationItemSelect
         setContentView(R.layout.activity_main)
         initData()
         initView()
-    }
-
-    private fun initView() {
-        buttonBackToolbar.visibility = View.GONE
-
-        initFragment()
     }
 
 }
--- app/src/main/java/com/jomhack/lendme/utils/Constants.kt
+++ app/src/main/java/com/jomhack/lendme/utils/Constants.kt
@@ -7,6 +7,18 @@ open class Constants {
         object BOARD {
             const val NAME = "fragmentBoard"
         }
+        object FRIENDS {
+            const val NAME = "fragmentFriends"
+        }
+        object INVESTS {
+            const val NAME = "fragmentInvests"
+        }
+        object HISTORY {
+            const val NAME = "fragmentHistory"
+        }
+        object SETTINGS {
+            const val NAME = "fragmentSetting"
+        }
     }
 
 }
