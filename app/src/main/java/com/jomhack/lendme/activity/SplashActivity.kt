212605996f122490e998921cec89ffd571ba53df
aldimaulana <aldieemaulana@gmail.com>
2018-10-06 13:08:15 +0800

    Adding SplashActivity

---
+++ app/src/main/java/com/jomhack/lendme/activity/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.jomhack.lendme.activity
+
+import com.jomhack.lendme.base.BaseActivity
+
+
+/**
+ * Created by Al on 06/10/2018 for JomHack
+ */
+
+class SplashActivity : BaseActivity() {
+
+}
