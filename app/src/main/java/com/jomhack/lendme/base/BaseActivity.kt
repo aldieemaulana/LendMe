4ac505bb7c4fa7d95df68cc7d4c73378d122da16
aldimaulana <aldieemaulana@gmail.com>
2018-10-06 13:07:42 +0800

    Adding Base Activity

---
+++ app/src/main/java/com/jomhack/lendme/base/BaseActivity.kt
@@ -0,0 +1,46 @@
+package com.jomhack.lendme.base
+
+import android.content.Context
+import android.graphics.Rect
+import android.support.v7.app.AppCompatActivity
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import com.kaopiz.kprogresshud.KProgressHUD
+
+
+/**
+ * Created by Al on 06/10/2018 for JomHack
+ */
+
+open class BaseActivity : AppCompatActivity() {
+
+    internal val context : Context = this
+    internal var progress: KProgressHUD? = null
+
+    /* for auto dismiss keyboard and clear focus when touch outside */
+    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+        if (event.action == MotionEvent.ACTION_DOWN) {
+            val v = currentFocus
+            if (v is EditText) {
+                val outRect = Rect()
+                v.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
+                    v.clearFocus()
+                    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(v.windowToken, 0)
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event)
+    }
+
+    fun showProgress() {
+        progress!!.show()
+    }
+
+    fun hideProgress() {
+        progress!!.dismiss()
+    }
+
+}
